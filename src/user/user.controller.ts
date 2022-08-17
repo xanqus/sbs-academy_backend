0b957994afb1457dec8e75a1b708d39d35601fb8
xanqus <sbk98031816@gmail.com>
2022-08-17 18:30:49 +0900

    users who did not submit studytime

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Get('/')
+  async getUser() {
+    return 'user';
+  }
+
+  @Get('/not-submit')
+  async getUserDidNotSubmit(
+    @Query('date') date,
+    @Query('lectureID') lectureID,
+  ) {
+    return this.userService.getUserDidNotSubmit(lectureID, date);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entity/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
+
+  async getUserDidNotSubmit(lectureID, date: any) {
+    return await this.userRepository.query(
+      `
+    SELECT *
+    FROM \`User\`
+    WHERE studentName
+    NOT IN (
+        SELECT studentName
+        FROM \`User\`
+        LEFT JOIN \`StudyTime\`
+        ON \`User\`.discordID = \`StudyTime\`.discordID
+        WHERE DATE(created_at) = ?
+    )
+    AND lectureID = ?
+    AND discordID
+    NOT IN (
+        '988298877058826282',
+        '784289256356839445',
+        '405361911115284490',
+        '784293332003848202'
+    )
+    `,
+      [date, lectureID],
+    );
+  }
+}
